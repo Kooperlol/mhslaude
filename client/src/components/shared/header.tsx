278dd0fa9153c23a2a73d40b33e625d2c82eb56f
Kooper <74805059+Kooperlol@users.noreply.github.com>
2023-11-07 12:57:12 -0600

    Updated footer & fixed mobie nav bar

--- client/src/components/shared/header.tsx
+++ client/src/components/shared/header.tsx
@@ -73,9 +73,6 @@ export default function Header() {
               ? "sm:hidden z-40 absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
               : "sm:hidden z-40 absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
           }
-          style={{
-            overflow: nav ? "hidden" : "auto",
-          }}
         >
           <ul>
             <li onClick={handleNav} className="p-4 text-4xl hover:text-primary">
